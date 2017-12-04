753b4139f0bfac69963f3e3d648e19dd2b92429f
davidcb <davidcouto@gmail.com>
2017-12-04 10:20:42 +0000

    Added more methods from the API

--- src/Davidcb/Esendex/Esendex.php
+++ src/Davidcb/Esendex/Esendex.php
@@ -10,17 +10,30 @@ class Esendex
     public function __construct($app)
     {
         $this->app = $app;
-        $this->authentication = $this->getAuthentication();
+        $this->authenticate();
+    }
+
+    /**
+     * Gives the ability to authenticate with different credentials if we
+     * have multiple accounts on our application. If not, it authenticates
+     * with the credentials on the config file.
+     * @param  string|null $reference
+     * @param  string|null $email
+     * @param  string|null $password
+     * @return null
+     */
+    public function authenticate($reference = null, $email = null, $password = null) {
+        $this->authentication = $this->getAuthentication($reference, $email, $password);
     }
 
     /**
      * Sends a message using the Esendex API
-     * @param  string $from      Sender's name (max 10 characters)
-     * @param  string $recipient Recipient's telephone number
-     * @param  string $text      SMS's body
-     * @return object            response
+     * @param  string $from             Sender's name (max 10 characters)
+     * @param  string $recipient        Recipient's telephone number
+     * @param  string $text             SMS's body
+     * @return Esendex\Model\ResultItem response
      */
-    public function sendMessage($from, $recipient, $text)
+    public function send($from, $recipient, $text)
     {
         $message = new \Esendex\Model\DispatchMessage(
             $from,
@@ -33,19 +46,54 @@ class Esendex
     }
 
     /**
-     * Returns inbox messages
-     * @return object|null       An object with the inbox messages
+     * Returns the number of credits on the current account
+     * @return int
      */
-    public function inboxMessages()
+    public function getCredits()
+    {
+        $service = new \Esendex\DispatchService($this->authentication);
+        return $service->getCredits();
+    }
+
+    /**
+     * Returns latest inbox messages
+     * @param int|null $startIndex
+     * @param int|null $count
+     * @return \Esendex\Model\InboxPage|null
+     */
+    public function latest($startIndex = null, $count = null)
     {
         $service = new \Esendex\InboxService($this->authentication);
-        return $service->latest();
+        return $service->latest($startIndex, $count);
+    }
+
+    /**
+     * Deletes an inbox message given its identifier
+     * @param  string $messageId
+     * @return bool
+     */
+    public function deleteInboxMessage($messageId)
+    {
+        $service = new \Esendex\InboxService($this->authentication);
+        return $service->deleteInboxMessage($messageId);
+    }
+
+    /**
+     * Update the read status of an inbox message given its identifier
+     * @param  string $messageId [description]
+     * @param  bool   $read
+     * @return bool
+     */
+    public function updateReadStatus($messageId, $read = true)
+    {
+        $service = new \Esendex\InboxService($this->authentication);
+        return $service->updateReadStatus($messageId, $read);
     }
 
     /**
      * Returns a given message status
      * @param  string $messageId The message's identifier
-     * @return string|null       The message's status string
+     * @return string|null
      */
     public function messageStatus($messageId)
     {
@@ -57,20 +105,85 @@ class Esendex
     /**
      * Returns a given message body
      * @param  string $messageId The message's identifier
-     * @return string            The message's full body
+     * @return string
      */
-    public function messageBody($messageId)
+    public function getMessageBodyById($messageId)
     {
         $service = new \Esendex\MessageBodyService($authentication);
         return $service->getMessageBodyById($messageId);
     }
 
-    protected function getAuthentication()
+    /**
+     * Returns latest sent messages
+     * @param int|null $startIndex
+     * @param int|null $count
+     * @return \Esendex\Model\SentMessagesPage|null
+     */
+    public function latestSent($startIndex = null, $count = null)
+    {
+        $service = new \Esendex\SentMessagesService($this->authentication);
+        return $service->latest($startIndex, $count);
+    }
+
+    /**
+     * Adds a telephone number to the opt-out list
+     * @param string $accountReference
+     * @param string $mobileNumber
+     * @return \Esendex\Model\OptOut
+     */
+    public function addToOptOut($accountReference, $mobileNumber)
+    {
+        $service = new \Esendex\OptOutsService($this->authentication);
+        return $service->add($accountReference, $mobileNumber);
+    }
+
+    /**
+     * Adds a telephone number to the opt-out list
+     * @param int $pageNumber
+     * @param int $pageSize
+     * @return array
+     */
+    public function getOptOutList($pageNumber = null, $pageSize = null)
+    {
+        $service = new \Esendex\OptOutsService($this->authentication);
+        return $service->get($pageNumber, $pageSize);
+    }
+
+    /**
+     * Returns the account with the given reference or the authenticated
+     * account reference if it exists
+     * @param  string $reference The account reference
+     * @return \Esendex\Model\Account|null
+     */
+    public function getAccount($reference = null)
+    {
+        $service = new \Esendex\AccountService($this->authentication);
+        return $service->getAccount($reference);
+    }
+
+    /**
+     * Returns the accounts within the authenticated account
+     * @return \Esendex\Model\Account
+     */
+    public function getAccounts()
+    {
+        $service = new \Esendex\AccountService($this->authentication);
+        return $service->getAccounts();
+    }
+
+    /**
+     * Gets the Esendex Authentication object for given credentials
+     * @param  string $reference Account reference
+     * @param  string $email     Authentication email address
+     * @param  string $password  Authentication password
+     * @return \Esendex\Authentication\LoginAuthentication
+     */
+    protected function getAuthentication($reference = null, $email = null, $password = null)
     {
         return new \Esendex\Authentication\LoginAuthentication(
-            $this->app['config']['esendex.account_id'],
-            $this->app['config']['esendex.email'],
-            $this->app['config']['esendex.password']
+            $reference ?? $this->app['config']['esendex.account_id'],
+            $email ?? $this->app['config']['esendex.email'],
+            $password ?? $this->app['config']['esendex.password']
         );
     }
 }
